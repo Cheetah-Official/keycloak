f86cf8f2a0142cb6cab74d024c57bc1844e4b15e
blurbeast <leumasre@gmail.com>
2025-12-02 00:59:31 +0100

    upff

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@
 export KC_HTTP_PORT=8080
 
 # Hostname configuration for reverse proxy
-export KC_HOSTNAME=keycloak-production-3369.up.railway.app
+# export KC_HOSTNAME=keycloak-production-3369.up.railway.app
 export KC_PROXY=edge
 ./bin/kc.sh start --optimized
