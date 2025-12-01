b2abb81108dac2e88bb651c6262c9711e06ccc07
blurbeast <leumasre@gmail.com>
2025-12-02 00:41:04 +0100

    updated

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+
+# Set Keycloak port to Railway-assigned port
+export KC_HTTP_PORT=8080
+
+# Hostname configuration for reverse proxy
+export KC_HOSTNAME=keycloak-production-3369.up.railway.app
+export KC_PROXY=edge
 ./bin/kc.sh start --optimized
